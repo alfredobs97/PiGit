41b79dd6971304c3ecf8c3945649946bcc5bd6b1
PragmaOnce <pcumpian1@gmail.com>
2019-10-01 17:30:40 +0200

    done

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
-	"os"
-	"os/exec"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -14,38 +12,4 @@ func main() {
 
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
-
-}
-func initGit(path string) error {
-	cmd := exec.Command("git", "init", "--bare")
-	cmd.Dir = path + ".git"
-
-	_, err := cmd.Output()
-
-	return err
-}
-func createDirectory(path string) error {
-	path = "/repos/" + path
-
-	err := os.MkdirAll(path+".git", os.ModePerm)
-
-	if err == nil {
-		err = initGit(path)
-	}
-
-	return err
-}
-
-func createRepo(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	dir := params["dir"]
-
-	err := createDirectory(dir)
-
-	if err == nil {
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "<h1> OK </h1>")
-	} else {
-		w.WriteHeader(503)
-	}
 }
